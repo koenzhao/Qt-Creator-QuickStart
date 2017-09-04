82bb0041ba025d9018fccbf59cd4ff47a448e7ad
koenchiu <koen.chiu@gmail.com>
2017-09-04 20:27:12 +0800

    learning chapter 3

---
+++ Chapter 3/3-1/myWidget1/main.cpp
@@ -0,0 +1,9 @@
+#include <QtGui>
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc,argv);
+    QWidget* widget = new QWidget();
+    widget->setWindowTitle("I'm widget");
+    QLabel* label = new QLabel();
+}
